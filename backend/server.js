c8146fd39c918ca0c568b826a07e250e2c814c39
a3ela <abelsintayehu626@gmail.com>
2025-10-14 18:03:20 +0300

    add: dummy products and api for serving with get request

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,21 @@
 const express = require("express");
-const PORT = 5000;
+const products = require("./products.js");
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.get("/", (request, response) => {
   response.send("api is running");
+});
+
+app.get("/api/products", (request, response) => {
+  response.json(products);
+});
+
+app.get("/api/products/:id", (request, response) => {
+    const product = products.find(p => p.id === request.params.id);
+    response.json(product);
 });
 
 app.listen(PORT, () => {
