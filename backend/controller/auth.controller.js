e463ba6417bce3ba9497d26349724773998aaa90
a3ela <abelsintayehu626@gmail.com>
2025-10-31 01:34:57 +0300

    removed unwanted import

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -2,59 +2,79 @@ const asyncHandler = require("../middleware/asyncHandler.js");
 const User = require("../models/user.model.js");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const { generateVerificationToken } = require("../utils/generateVerificationToken.js");
+const {
+  generateVerificationToken,
+} = require("../utils/generateVerificationToken.js");
 const generateTokenAndSetCookie = require("../utils/generateTokenAndSetCookie.js");
-const { sendVerificationEmail, sendWelcomeEmail, sendPasswordResetEmail, sendResetSuccessEmail } = require("../mailtrap/emails.js");
+const {
+  sendVerificationEmail,
+  sendWelcomeEmail,
+  sendPasswordResetEmail,
+  sendResetSuccessEmail,
+} = require("../mailtrap/emails.js");
 const crypto = require("crypto");
 
 require("dotenv").config();
 
-
 const signup = asyncHandler(async (request, response) => {
-    const {email, password, name} = request.body;
+  const { email, password, name } = request.body;
 
-    if (!email || !password || !name) {
-        response.status(400);
-        throw new Error("Please provide all required fields");
-    }
+  if (!email || !password || !name) {
+    response.status(400);
+    throw new Error("Please provide all required fields");
+  }
 
-    const userExists = await User.findOne({ email });
-    if (userExists) {
-       return response.status(400).json({success: false, message: "User already exists" });
-    }
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    return response
+      .status(400)
+      .json({ success: false, message: "User already exists" });
+  }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const verificationToken = generateVerificationToken();
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const verificationToken = generateVerificationToken();
 
-    const user = User({
-        name,
-        email,
-        password: hashedPassword,
-        verificationToken,
-        verificationTokenExpireAt: Date.now() + 24 * 60 * 1000, // 24 hours
+  const user = User({
+    name,
+    email,
+    password: hashedPassword,
+    verificationToken,
+    verificationTokenExpireAt: Date.now() + 24 * 60 * 1000, // 24 hours
+  });
+
+  await user.save();
+
+  // jwt
+  generateTokenAndSetCookie(response, user.id);
+
+  await sendVerificationEmail(user.email, user.verificationToken);
+
+  response
+    .status(201)
+    .json({
+      success: true,
+      message: "User registered successfully. Please verify your email.",
+      user: { id: user.id, name: user.name, email: user.email },
     });
-
-    await user.save();
-
-    // jwt
-    generateTokenAndSetCookie(response, user.id);
-
-    await sendVerificationEmail(user.email, user.verificationToken);
-
-    response.status(201).json({ success: true, message: "User registered successfully. Please verify your email.", user: { id: user.id, name: user.name, email: user.email }});
-
-    
 });
 
 const verifyEmail = asyncHandler(async (request, response) => {
   const { code } = request.body;
 
-  const user = await User.findOne({ verificationToken: code, verificationTokenExpireAt: { $gt: Date.now() } });
+  const user = await User.findOne({
+    verificationToken: code,
+    verificationTokenExpireAt: { $gt: Date.now() },
+  });
 
   if (!user) {
-    return response.status(400).json({ success: false, message: "Invalid or expired verification code." });
+    return response
+      .status(400)
+      .json({
+        success: false,
+        message: "Invalid or expired verification code.",
+      });
   }
-  
+
   user.isVerified = true;
   user.verificationToken = undefined;
   user.verificationTokenExpireAt = undefined;
@@ -69,16 +89,13 @@ const verifyEmail = asyncHandler(async (request, response) => {
 const login = asyncHandler(async (request, response) => {
   const { email, password } = request.body;
 
-  const user = await User.findOne({email});
+  const user = await User.findOne({ email });
+  const isValid = user && (await bcrypt.compare(password, user.password));
 
-  if (!user) {
-    return response.status(400).json({success: false, message: "Invalid credentials"})
-  }
-
-  const isPasswordValid = await bcrypt.compare(password, user.password);
-
-  if (!isPasswordValid) {
-    return response.status(400).json({success: false, message: "Invalid credentials"})
+  if (!isValid) {
+    return response
+      .status(400)
+      .json({ success: false, message: "Invalid email or password" });
   }
 
   generateTokenAndSetCookie(response, user.id);
@@ -86,7 +103,13 @@ const login = asyncHandler(async (request, response) => {
   user.lastLogin = new Date();
   await user.save();
 
-  response.status(200).json({ success: true, message: "Logged in successfully.", user: { id: user.id, name: user.name, email: user.email }});
+  response
+    .status(200)
+    .json({
+      success: true,
+      message: "Logged in successfully.",
+      user: { id: user.id, name: user.name, email: user.email },
+    });
 });
 
 const logout = asyncHandler(async (request, response) => {
@@ -98,18 +121,26 @@ const forgotPassword = asyncHandler(async (request, response) => {
   const { email } = request.body;
 
   const user = await User.findOne({ email });
-  
+
   if (!user) {
-    return response.status(400).json({ success: false, message: "User with this email does not exist." });
+    return response
+      .status(400)
+      .json({
+        success: false,
+        message: "User with this email does not exist.",
+      });
   }
 
   // Here you would generate a password reset token and send an email
-  const resetToken = crypto.randomBytes(20).toString('hex');
+  const resetToken = crypto.randomBytes(20).toString("hex");
   user.resetPasswordToken = resetToken;
   user.resetPasswordExpireAt = Date.now() + 60 * 60 * 1000; // 1 hour
   await user.save();
 
-  await sendPasswordResetEmail(user.email, `${process.env.CLIENT_URL}/reset-password/${resetToken}`);
+  await sendPasswordResetEmail(
+    user.email,
+    `${process.env.CLIENT_URL}/reset-password/${resetToken}`
+  );
 
   response.json({ success: true, message: "Password reset email sent." });
 });
@@ -117,25 +148,43 @@ const forgotPassword = asyncHandler(async (request, response) => {
 const resetPassword = asyncHandler(async (request, response) => {
   const { token } = request.params;
   const { password } = request.body;
- console.log("token: ", token, "newpassword: ", password);
-  const user = await User.findOne({ resetPasswordToken: token, resetPasswordExpireAt: { $gt: Date.now() } });
-  
+  console.log("token: ", token, "newpassword: ", password);
+  const user = await User.findOne({
+    resetPasswordToken: token,
+    resetPasswordExpireAt: { $gt: Date.now() },
+  });
+
   if (!user) {
-    return response.status(400).json({ success: false, message: "Invalid or expired password reset token." });
+    return response
+      .status(400)
+      .json({
+        success: false,
+        message: "Invalid or expired password reset token.",
+      });
   }
   const hashedPassword = await bcrypt.hash(password, 10);
-console.log('here')
+  console.log("here");
   user.password = hashedPassword;
   user.resetPasswordToken = undefined;
   user.resetPasswordExpireAt = undefined;
   await user.save();
-  
+
   await sendResetSuccessEmail(user.email);
-  response.json({ success: true, message: "Password has been reset successfully." });
+  response.json({
+    success: true,
+    message: "Password has been reset successfully.",
+  });
 });
 
 const checkAuth = asyncHandler(async (request, response) => {
-  const user = await User.findById(request.userId).select('-password');
-})
+  const user = await User.findById(request.userId).select("-password");
+});
 
-module.exports = { signup, login, logout, verifyEmail, forgotPassword, resetPassword };
+module.exports = {
+  signup,
+  login,
+  logout,
+  verifyEmail,
+  forgotPassword,
+  resetPassword,
+};
